bf01c9d527a05c01512aa0f9e4108ea4bce7c814
Ze Vitor <josevitordeneto@gmail.com>
2025-10-15 06:41:04 -0300

    vendo se ta sincronizado

--- scripts/3_Distribuição normal exercícioss.r
+++ scripts/3_Distribuição normal exercícioss.r
@@ -32,7 +32,7 @@ y <- dnorm(x)
 #Aqui temos a densidade de probabilida de um valor menor que 1.42 e maior que zero, calculada a partir da integral 
 pnorm(a) - pnorm(b)
 
-
+#Plota x e y com 
 plot(x,  
      y,
      type = 'l',
@@ -46,7 +46,33 @@ polygon(c(x_area, rev(x_area)),
         c(dnorm(x_area),rep(0, length(x_area))), 
         col = 'steelblue')
 
-#Crie a sua propria curva 
+#Crie a sua propria curva
 
+## Dado que uma população tem média de 4,6g/dl e devsio padrão de 0.5g/dl
+#rnorm() gera uma distriubuição normal, de tamanho n, com parâmetros para média 
+# e desvio padrão
+
+a = 180
+b = 195
+
+#Aqui temos os valores maiores e menores, bem como o intervalo entre eles
+x <- seq((-4*10)+200, (4*10)+200, 0.001)
+y <- dnorm(x, 200, 10)
+
+#Aqui temos a densidade de probabilida de um valor menor que 1.42 e maior que zero, calculada a partir da integral 
+#pnorm(a) - pnorm(b)
+
+#Plota x e y com 
+plot(x,  
+     y,
+     type = 'l',
+     main = "", 
+     xlab = "", ylab = "")
+
+x_area = x[x>=a & x<=b]
+
+polygon(c(x_area, rev(x_area)),
+        c(dnorm(x_area),rep(0, length(x_area))), 
+        col = 'steelblue')
 
 
